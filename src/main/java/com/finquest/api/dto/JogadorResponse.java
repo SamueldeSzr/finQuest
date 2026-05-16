8618b395a2733870609f903a08240a80e440eb65
IFelipeLGZI <felipevieiraoficialbr@gmail.com>
2026-05-15 23:14:27 -0300

    atualiza todos os arquivos existentes com autenticação JWT

--- src/main/java/com/finquest/api/dto/JogadorResponse.java
+++ src/main/java/com/finquest/api/dto/JogadorResponse.java
@@ -1,4 +1,10 @@
 package com.finquest.api.dto;
 
-public class JogadorResponse {
-}
+public record JogadorResponse(
+        Long id,
+        String nomePlayer,
+        String email,
+        int nivelAtual,
+        int xpPlayer,
+        int vidasJogador
+) {}
